422e7b92f9d4dfb595b26368c02cebf5f641a6f8
DESKTOP-HGRT63B\Ahmad Mnasra <ahmad.mnasra@gmail.com>
2017-07-29 11:37:24 +0300

    add my js

--- finalWebSite/js/myjs.js
+++ finalWebSite/js/myjs.js
@@ -1,9 +1,6 @@
 ﻿function EditMode(el, isEdit) {
     $("tbody td input").hide();
 
-    if ($("tbody td input").val == '') {
-        $("tbody td span").html("0");
-    }
 
     $("tbody td span").show();
     if (isEdit) {
